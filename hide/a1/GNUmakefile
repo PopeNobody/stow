f88c91bc9088ac0d87523cc52fd1aab557296b60
nobody <github2@invisiblehand.church>
2020-07-31 10:05:36 -0400

    x

---
+++ hide/a1/GNUmakefile
@@ -0,0 +1,5 @@
+
+MAKE_FLAGS:=	-Rr --warn-undefined-variable 
+
+default:
+	./run_make ${MAKE} -f Makefile ${MAKE_FLAGS} ${MAKE_CMD_GOALS}
---
+++ hide/a1/fuck.sh
@@ -0,0 +1,10 @@
+church                        bin                       doc
+etc                           GNUmakefile               hide
+higt                          lib                       log
+Makefile                      nn-cripple                nn-laptop
+nn-yy                         run_make                  sbin
+master                        nn-cripple                nn-desktop
+nn-laptop                     nn-yy                     root_standard
+remotes/origin/church         remotes/origin/master     remotes/origin/nn-cripple
+remotes/origin/nn-desktop     remotes/origin/nn-laptop  remotes/origin/nn-yy
+remotes/origin/root_standard                            
