2a3633e2c4e08ed6104f44a8fd92b13d711f8bb4
nobody <github2@invisiblehand.church>
2024-03-19 07:48:54 -0400

    x

--- lib/perl/Util.pm
+++ lib/perl/Util.pm
@@ -2,7 +2,7 @@
 # vim: ts=2 sw=2 ft=perl
 package Util;
 our ( @EXPORT, @EXPORT_OK, @ISA );
-use vars qw(@fb @dd);
+use vars qw(@dd);
 BEGIN {
   @pp=qw( pp dd ppx ddx quote qquote );
 };
@@ -14,26 +14,25 @@ BEGIN {
   push(@EXPORT_OK,@FindBin::EXPORT_OK);
   push(@EXPORT_OK,@Nobody::PP::EXPORT_OK);
   push(@EXPORT_OK,
-    qw( sum avg max min mkdir_p basename suckdir suck spit )
+    qw( sum avg max min mkdir_p basename suckdir suck spit maybeRef )
+  );
+  push(@EXPORT_OK,
+    qw( pasteLines )
   );
 }
 use strict;
 use warnings;
 use autodie;
 use Env qw( $HOME $PWD @PATH );
-use FindBin @fb=qw( $Bin $Script);
 use lib "/opt/lib/perl";
 use lib "$HOME/lib/perl";
 BEGIN {
   @EXPORT=@EXPORT_OK;
   @ISA=qw(Exporter);
   require Exporter;
-  sub import;
-  *import=\&Exporter::import; 
-  # sub import {
-  #   $DB::single=1;
-  #   goto &Exporter::import;
-  # };
+  sub import {
+    goto &Exporter::import;
+  };
 }
 sub sum(@){
   my $sum=0;     
@@ -47,7 +46,7 @@ sub avg(@){
 sub max(@){
   my $max=shift;
   for(grep { defined } @_) {
-    $max=$_ if $max>$_;
+    $max=$_ if $max<$_;
   };
   return $max;
 }
@@ -67,15 +66,32 @@ sub basename {
   s{.*/}{} if m{/.};
   return $_;
 }
+sub suckdir(@);
 sub suckdir(@){
-  return grep { !m{/[.][.]?$} } map { glob("$_/* $_/.*") } @_;
+  return map { suckdir($_) } @_ unless @_ == 1;
+  for(shift) {
+    die "undefined dirname" unless defined;
+    if(length) {
+      return grep { !m{/[.][.]?$} } glob("$_/* $_/.*");
+    } else {
+      return grep { s{^\./}{} } suckdir(".");
+    };
+  }
 }
 sub suck(@){
   return warn("useless use of suck in void context") unless defined wantarray;
-  return () unless @_;
   local(@ARGV)=@_;
-  return <ARGV>;
+  local(@_)=<ARGV>;
+  return join("",@_) unless wantarray;
+  return @_;
 };
+sub pasteLines(@) {
+  for(join("",@_)){
+    s{\\\n?$}{}sm;
+  }
+  return join("\n",@_) unless wantarray;
+  return @_;
+}
 sub spit($@){
   local($\,$/);
   my ($fn,$fh)=shift;
@@ -84,5 +100,8 @@ sub spit($@){
   open($fh,$fn);
   $fh->print($_) for join("",@_);
   close($fh);
+};
+sub maybeRef($) {
+  return ref || $_ for shift;
 };
 1;
